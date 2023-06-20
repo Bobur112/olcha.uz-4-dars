56f66427bb183fd10f4fead828041ffd691d2ab0
Bobur112 <bahrommahmudov112@gmail.com>
2023-06-20 16:53:28 +0500

    yuklash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="shadow">
+        <div className="container-fluid py-3">
+          <img src="https://olcha.uz/_nuxt/logo-red.e48e0ab8.svg" alt="" />
+        </div>
       </header>
+
+      <section id="products" className="py-3">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/supplier/stores/1/2023-06-14/FQnnYx2U48BFhkVAOTZElwI2rPDOAvlAojtRklhLIqxxJbW1yBDkWiLBjXAR.jpg"
+                  alt=""
+                />
+                <a href="#" className="title">
+                  Huawei Nova 11i 8/128GB Black{" "}
+                </a>
+                <p className="price mt-3 mb-0">3 059 000 so'm</p>
+                <p className="priceMonth">413 000 so'm x 12 oy</p>
+                <a href="#" className="btn">
+                  Muddatli to'lov
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/supplier/stores/1/2023-06-14/FQnnYx2U48BFhkVAOTZElwI2rPDOAvlAojtRklhLIqxxJbW1yBDkWiLBjXAR.jpg"
+                  alt=""
+                />
+                <a href="#" className="title">
+                  Huawei Nova 11i 8/128GB Black{" "}
+                </a>
+                <p className="price mt-3 mb-0">3 059 000 so'm</p>
+                <p className="priceMonth">413 000 so'm x 12 oy</p>
+                <a href="#" className="btn">
+                  Muddatli to'lov
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/supplier/stores/1/2023-06-14/FQnnYx2U48BFhkVAOTZElwI2rPDOAvlAojtRklhLIqxxJbW1yBDkWiLBjXAR.jpg"
+                  alt=""
+                />
+                <a href="#" className="title">
+                  Huawei Nova 11i 8/128GB Black{" "}
+                </a>
+                <p className="price mt-3 mb-0">3 059 000 so'm</p>
+                <p className="priceMonth">413 000 so'm x 12 oy</p>
+                <a href="#" className="btn">
+                  Muddatli to'lov
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/supplier/stores/1/2023-06-14/FQnnYx2U48BFhkVAOTZElwI2rPDOAvlAojtRklhLIqxxJbW1yBDkWiLBjXAR.jpg"
+                  alt=""
+                />
+                <a href="#" className="title">
+                  Huawei Nova 11i 8/128GB Black{" "}
+                </a>
+                <p className="price mt-3 mb-0">3 059 000 so'm</p>
+                <p className="priceMonth">413 000 so'm x 12 oy</p>
+                <a href="#" className="btn">
+                  Muddatli to'lov
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/supplier/stores/1/2023-06-14/FQnnYx2U48BFhkVAOTZElwI2rPDOAvlAojtRklhLIqxxJbW1yBDkWiLBjXAR.jpg"
+                  alt=""
+                />
+                <a href="#" className="title">
+                  Huawei Nova 11i 8/128GB Black{" "}
+                </a>
+                <p className="price mt-3 mb-0">3 059 000 so'm</p>
+                <p className="priceMonth">413 000 so'm x 12 oy</p>
+                <a href="#" className="btn">
+                  Muddatli to'lov
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
